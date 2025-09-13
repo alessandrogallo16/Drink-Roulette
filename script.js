25cd3c1380a63f7e32a45d5025d2bb80039d3536
alessandrogallo16 <galloalessandro1609@gmail.com>
2025-09-13 12:29:32 +0200

    ho reso gli ingredienti cliccabili per far una ricerca su google

--- script.js
+++ script.js
@@ -15,72 +15,76 @@ const newDrinkBtn = document.querySelector(".new-drink-btn")
 const cardLink = document.querySelector(".card-link")
 
 
-function loading () {
+function loading() {
 
     randomSection.setAttribute("class", "random-drink d-none")
     loadingDiv.setAttribute("class", "container loading loading-div")
-    
+
 }
 
-function stopLoading () {
+function stopLoading() {
 
     randomSection.setAttribute("class", "random-drink")
     loadingDiv.setAttribute("class", "container d-none loading loading-div")
 
 }
 
-function stopLoadingErr () {
+function stopLoadingErr() {
     loadingDiv.setAttribute("class", "container d-none loading loading-div")
     errorDiv.setAttribute("class", "container loading error-div")
 }
 
-function refreshPage () {
+function refreshPage() {
     location.reload()
 }
 
 fetch(SERVER_ADDRESS1)
-    .then(result => { 
+    .then(result => {
         loading()
-        return result.json() 
-         })
+        return result.json()
+    })
     .then(json => json.drinks[0])
     .then(drink => {
         cardImg.setAttribute("src", drink.strDrinkThumb)
         cardTitle.innerText = drink.strDrink;
         cardText.innerText = drink.strInstructionsIT;
-            for (i = 1; i < 16; i++) {
-                if (drink[`strIngredient${i}`] !== null) {
-                    drinkType.innerText = drink.strAlcoholic === "Alcoholic" ? "Alcolico" : "Non alcolico"
+        for (i = 1; i < 16; i++) {
+            if (drink[`strIngredient${i}`] !== null) {
+                drinkType.innerText = drink.strAlcoholic === "Alcoholic" ? "Alcolico" : "Non alcolico"
 
-                    let newItem = document.createElement('li')
-                    newItem.setAttribute("class", "list-group-item")
-                    ingredient = drink[`strIngredient${i}`]
-                    dosaggio = drink[`strMeasure${i}`]
-                        if (dosaggio === null) {
-                            dosaggio = ""
-                        }
-                    
-                    newItem.innerHTML =  `<div class="row"> <div class="col">${ingredient}</div> <div class="col text-end">${dosaggio}</div></div>`
-                    listGroup.appendChild(newItem)
-                    cardLink.setAttribute("href", GOOGLE_SEARCH + drink.strDrink)
-                    stopLoading()
+                let newItem = document.createElement('li')
+                newItem.setAttribute("class", "list-group-item")
+                let ingredient = drink[`strIngredient${i}`]
+                let dosaggio = drink[`strMeasure${i}`]
+                if (dosaggio === null) {
+                    dosaggio = ""
+                }
+                newItem.innerHTML = `<div class="row"> <div class="col ingredient-class">${ingredient}</div> <div class="col text-end">${dosaggio}</div></div>`
+                listGroup.appendChild(newItem)
+                cardLink.setAttribute("href", GOOGLE_SEARCH + drink.strDrink)
 
-                   
-                }    
-            }   
-        
-        })
-    .catch(error => { 
-        console.log(error + `  errore nel reperire le informazioni dal server (API FETCH)`)
+                let ingredientUrl = GOOGLE_SEARCH + ingredient
+                let ingredientDiv = newItem.querySelector('.ingredient-class')
+                ingredientDiv.style.cursor = 'pointer'
+                ingredientDiv.addEventListener('click', () => {
+                    window.open(ingredientUrl, '_blank')
+                })
+
+
+                
+            }
+        }
+        stopLoading()
+
+    })
+    .catch(error => {
+        console.log(error)
         stopLoadingErr()
-
-
-
     })
 
 
 
-        
+
 newDrinkBtn.addEventListener("click", refreshPage)
 
 
