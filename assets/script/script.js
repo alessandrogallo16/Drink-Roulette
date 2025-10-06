f169aad17e74c71ff204fa3c82e9b83209546394
alessandrogallo16 <galloalessandro1609@gmail.com>
2025-10-06 22:48:20 +0200

    aggiunti vari drink + pulsante per segnalare un problema. tolto alert dei lavori in corso

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,18 +1,19 @@
-const SERVER_ADDRESS1 = "https://www.thecocktaildb.com/api/json/v1/1/random.php?units=metric"
+const DATABASE_ADDRESS = "./assets/data/drinks.json"
 const GOOGLE_SEARCH = "https://www.google.com/search?q="
 
 
 const randomSection = document.querySelector(".random-drink")
 const cardTitle = document.querySelector(".card-title")
 const cardText = document.querySelector(".card-text")
-const listGroup = document.querySelector(".list-group")
-const listItems = document.querySelectorAll(".list-group-item")
+const listIng = document.querySelector(".list-group")
+const listItem = document.querySelectorAll(".list-group-item")
 const cardImg = document.querySelector(".card-img-top")
 const drinkType = document.querySelector(".drink-type")
 const loadingDiv = document.querySelector(".loading-div")
 const errorDiv = document.querySelector(".error-div")
 const newDrinkBtn = document.querySelector(".new-drink-btn")
 const cardLink = document.querySelector(".card-link")
+
 
 
 function loading() {
@@ -38,42 +39,92 @@ function refreshPage() {
     location.reload()
 }
 
-fetch(SERVER_ADDRESS1)
+function getRandomItem(array) {
+    const index = Math.floor(Math.random() * array.length)
+    return array[index]
+}
+
+/* la funzione "convertIdText" serve per convertire l'id del dell'ingrediente del drink attuale, nel nome stesso dell' ingrediente;
+    per poi aggiungere la ricerca automatica su google se viene cliccato il nome*/
+
+function convertIdText() {
+
+    fetch(DATABASE_ADDRESS)
+        .then(result => {
+            return result.json()
+        })
+        .then(data => {
+            const ingToConvert = document.querySelectorAll(".ingredient-class")
+            for (ing of ingToConvert) {
+                for (i of data.ingredienti) {
+                    if (i.id == ing.innerText) {
+                        console.log("trovato id che corrisponde a ingrediente del drink attuale")
+                        ing.innerText = i.nome
+                    }
+                }
+            }
+
+
+            const ingToSearch = document.querySelectorAll(".ingredient-class")
+            for (singleIng in ingToSearch) {
+
+                const actualIng = ingToSearch[singleIng]
+                const ingUrl = GOOGLE_SEARCH + actualIng.innerText
+                console.log(ingUrl)
+
+                function Search() {
+                    return window.open(ingUrl, '_blank')
+                }
+                actualIng.style.cursor = 'pointer'
+                actualIng.addEventListener('click', Search)
+            }
+        })
+        .catch(errore => console.log(errore))
+}
+
+
+
+function fillCardIng(arrIng) {
+    for (ing of arrIng) {
+        const newIng = document.createElement("li")
+        newIng.innerHTML = `<div class="row"> <div class="col ingredient-class">${ing.ingredienteId}</div> <div class="col text-end">${ing.quantita}</div></div>`
+        listIng.append(newIng)
+    }
+}
+
+
+/* modifica del DOM e impaginazione delle informazioni */
+
+fetch(DATABASE_ADDRESS)
     .then(result => {
         loading()
         return result.json()
     })
-    .then(json => json.drinks[0])
-    .then(drink => {
-        cardImg.setAttribute("src", drink.strDrinkThumb)
-        cardTitle.innerText = drink.strDrink;
-        cardText.innerText = drink.strInstructionsIT;
-        for (i = 1; i < 16; i++) {
-            if (drink[`strIngredient${i}`] !== null) {
-                drinkType.innerText = drink.strAlcoholic === "Alcoholic" ? "Alcolico" : "Non alcolico"
-
-                let newItem = document.createElement('li')
-                newItem.setAttribute("class", "list-group-item")
-                let ingredient = drink[`strIngredient${i}`]
-                let dosaggio = drink[`strMeasure${i}`]
-                if (dosaggio === null) {
-                    dosaggio = ""
-                }
-                newItem.innerHTML = `<div class="row"> <div class="col ingredient-class">${ingredient}</div> <div class="col text-end">${dosaggio}</div></div>`
-                listGroup.appendChild(newItem)
-                cardLink.setAttribute("href", GOOGLE_SEARCH + drink.strDrink + " cocktail")
-
-                let ingredientUrl = GOOGLE_SEARCH + ingredient 
-                let ingredientDiv = newItem.querySelector('.ingredient-class')
-                ingredientDiv.style.cursor = 'pointer'
-                ingredientDiv.addEventListener('click', () => {
-                    window.open(ingredientUrl, '_blank')
-                })
+    .then(data => {
+        console.log(data.ingredienti)
+        console.log(data.cocktails)
 
 
-                
-            }
-        }
+        const newDrink = getRandomItem(data.cocktails)
+        console.log(newDrink)
+
+        cardImg.setAttribute("src", "." + newDrink.immagine.url)
+        cardImg.setAttribute("alt", newDrink.immagine.alt)
+
+        cardTitle.innerText = newDrink.nome
+        drinkType.innerText = "Alcolico"
+        cardText.innerText = newDrink.istruzioni
+        fillCardIng(newDrink.ingredienti)
+        convertIdText()
+
+
+
+
+
+
+
+
+        cardLink.setAttribute("href", GOOGLE_SEARCH + newDrink.nome + " cocktail")
         stopLoading()
 
     })
@@ -86,6 +137,3 @@ fetch(SERVER_ADDRESS1)
 
 
 newDrinkBtn.addEventListener("click", refreshPage)
-
-
-
